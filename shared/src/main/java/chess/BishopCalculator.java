d1d24a0c99a9cc65e847515f3b35ead1ec47cb3a
Fiery51 <tylerjackmo@outlook.com>
2026-01-22 00:11:36 -0700

    Update BishopCalculator.java

--- shared/src/main/java/chess/BishopCalculator.java
+++ shared/src/main/java/chess/BishopCalculator.java
@@ -5,33 +5,40 @@ import java.util.List;
 
 public class BishopCalculator {
     ArrayList<ChessMove> moves = new ArrayList<ChessMove>();
+    ChessPosition initialPosition; 
 
     public Collection<ChessMove> returnAllMoves(ChessBoard board, ChessPosition myPosition){
         //return List.of(new ChessMove(myPosition, endPositions(myPosition), null));
+        initialPosition = myPosition; 
         endPositions(myPosition);
         return moves; 
     }
 
     private ChessPosition endPositions(ChessPosition ChessPosition){
+        int num1;
+        int num2;
+        if( !((ChessPosition.getRow() + 1 <= 8)  && (ChessPosition.getRow() - 1 >= 1) && (ChessPosition.getColumn() <= 8) && (ChessPosition.getColumn() >= 1))){
+            return ChessPosition; 
+        }
         //top right
         if((ChessPosition.getRow() + 1 <= 8) && (ChessPosition.getColumn() + 1 <= 8)){
-            moves.add(new ChessMove(ChessPosition, new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() + 1), null));
-            return endPositions(new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() + 1));
+            moves.add(new ChessMove(initialPosition, new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() + 1), null));
+            endPositions(new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() + 1));
         }
         ////top left
-        if((ChessPosition.getRow() - 1 >= 1) && (ChessPosition.getColumn() + 1 <= 8)){
-            moves.add(new ChessMove(ChessPosition, new ChessPosition(ChessPosition.getRow() - 1, ChessPosition.getColumn() + 1), null));
-            return endPositions(new ChessPosition(ChessPosition.getRow() - 1, ChessPosition.getColumn() + 1));
+        else if((ChessPosition.getRow() - 1 >= 1) && (ChessPosition.getColumn() + 1 <= 8)){
+            moves.add(new ChessMove(initialPosition, new ChessPosition(ChessPosition.getRow() - 1, ChessPosition.getColumn() + 1), null));
+            endPositions(new ChessPosition(ChessPosition.getRow() - 1, ChessPosition.getColumn() + 1));
         }
         ////bottom right
-        if((ChessPosition.getRow() + 1 <= 8) && (ChessPosition.getColumn() - 1 >= 1)){
-            moves.add(new ChessMove(ChessPosition, new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() - 1), null));
-            return endPositions(new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() + 1));
+        else if((ChessPosition.getRow() + 1 <= 8) && (ChessPosition.getColumn() - 1 >= 1)){
+            moves.add(new ChessMove(initialPosition, new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() - 1), null));
+            endPositions(new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() - 1));
         }
         ////bottom left
-        if((ChessPosition.getRow() - 1 >= 1) && (ChessPosition.getColumn() - 1 >= 1)){
-            moves.add(new ChessMove(ChessPosition, new ChessPosition(ChessPosition.getRow() - 1, ChessPosition.getColumn() - 1), null));
-            return endPositions(new ChessPosition(ChessPosition.getRow() + 1, ChessPosition.getColumn() + 1));
+        else if((ChessPosition.getRow() - 1 >= 1) && (ChessPosition.getColumn() - 1 >= 1)){
+            moves.add(new ChessMove(initialPosition, new ChessPosition(ChessPosition.getRow() - 1, ChessPosition.getColumn() - 1), null));
+            endPositions(new ChessPosition(ChessPosition.getRow() - 1, ChessPosition.getColumn() - 1));
         }
         //itself
         return ChessPosition; 
