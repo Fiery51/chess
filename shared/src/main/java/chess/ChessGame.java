2581e35db31a139d15d60b7deb7c6df108c8613d
Fiery51 <tylerjackmo@outlook.com>
2026-02-02 23:41:46 -0700

    Commenting out and planning

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -56,13 +56,13 @@ public class ChessGame {
         //Grab the chess piece currently at whatever spot is at startPosition
 
         //from the current board, we want to get the piece at the start position, and return all valid moves that piece can make just for right now 
-        if(getBoard().getPiece(startPosition) == null){
-            return null; 
-        }
-        //else{
-        //    return getBoard().getPiece(startPosition).pieceMoves(currentChessBoard, startPosition);
+        //if(currentChessBoard.getPiece(startPosition) != null){
+            return getBoard().getPiece(startPosition).pieceMoves(currentChessBoard, startPosition);
         //}
-        return new ArrayList<ChessMove>();
+        //else{
+        //    return null;
+        //}
+        //return new ArrayList<ChessMove>();
     }
 
     /**
@@ -88,6 +88,43 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
+        //Overall layout of the function
+        //First check if the position is empty, if so skip it
+
+        //Second if it isn't lets grab that piece's team color. If it is NOT EQUAL to teamColor, and it is NOT a king, then run its pieceMoves function and add that to an arraylist we're making
+
+        //Third keep doing above (somehow work out the pawns diagonal, might just have to manually code that in? *sigh*)
+
+        //Fourth, now grab our current teamColors kings position (IF WE FOUND IT KEEP TRACK OF IT), and see if that position is IN that arraylist that we we made of all those moves of the opposite team
+        //IF YES, return true. If NO return false
+
+
+
+
+
+
+        //Idea:
+        //Lets just loop through every single piece on the board. Top to bottom, left to right
+        //Then i think we use the chess piece java file, and we can grab every single valid move that those pieces can make
+        //and create one GIANT list of those. Then we can see if the kings position is in that list? If so return true, else return false
+
+
+
+
+//________________________________________________
+        //OHHH wait, probably make ALLL of that portion, and that overall layout a function.
+        //We'll need to do the same thing for isInCheckmate. But for isInCheck that means they can move out of check,
+        //so we need to also run this "is this square safe" check on the surrounding squares of the king, so see if the king
+        //can move to any of the surrounding squares. If not we know "hey the king can't move to any of the surrounding squares"
+        //Then we should probably see "hey can any of the teamate pieces block the attacking piece(s)." Gah holy smokes.
+        //Hmmm this isn't all done in "isInCheck", this is like the entire validMoves logic lmao. But this is why cloning boards is important
+        
+
+
+        //OKAY so seperation of duties, look into this!!
+
+
+
         return true; 
     }
 
@@ -98,6 +135,7 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
+        
         return true; 
     }
 
