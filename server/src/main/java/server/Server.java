6e1f6805c272f0534ebeb22dcdb80bca89686738
Fiery51 <tylerjackmo@outlook.com>
2026-02-24 01:32:58 -0700

    aha

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,7 +10,13 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
-
+        //javalin.post("/user", null);
+        //javalin.post("/session", null);
+        //javalin.delete("session", null);
+        //javalin.get("/game", null);
+        //javalin.post("/game", null);
+        //javalin.put("/game", null);
+        javalin.delete("/db", null);
     }
 
     public int run(int desiredPort) {
