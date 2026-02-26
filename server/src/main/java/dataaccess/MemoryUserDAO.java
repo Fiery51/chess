8f1701cacbaf9298f676291f994e0dbc6ea6332d
Fiery51 <tylerjackmo@outlook.com>
2026-02-26 16:31:29 -0700

    started implenetation

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,5 +1,13 @@
 package dataaccess;
 
+import model.UserData;
+
 public class MemoryUserDAO implements UserDAO{
-    
+    public void findUser(String username){
+
+    }
+
+    public void addUser(UserData userData){
+
+    }
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,5 +1,9 @@
 package dataaccess;
 
+import model.UserData;
+
 public interface UserDAO {
+    void findUser(String username);
+    void addUser(UserData userData);
     
 }
