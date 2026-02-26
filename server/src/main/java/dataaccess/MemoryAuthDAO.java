d5e6d618062ab07d27bc64632ba69e349413f263
Fiery51 <tylerjackmo@outlook.com>
2026-02-26 16:39:44 -0700

    Update MemoryAuthDAO.java

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,5 +1,27 @@
 package dataaccess;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import model.AuthData;
+
 public class MemoryAuthDAO implements AuthDAO{
+    //authtoken, username
+    Map<String, String> dataBase = new HashMap<>(); 
     
+    public void addAuth(AuthData authData){
+        dataBase.put(authData.getAuthToken(), authData.getUsername());
+    }
+
+    public void deleteAuth(AuthData authData){
+        dataBase.remove(authData.getAuthToken());
+    }
+
+    public boolean validateAuth(AuthData authData){
+        return dataBase.containsKey(authData.getAuthToken());
+    }
+
+    public void clearData(){
+        dataBase = new HashMap<>(); 
+    }
 }
