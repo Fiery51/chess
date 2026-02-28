0fbf8485afd550d3dff754d816f7b004d04ff593
Fiery51 <tylerjackmo@outlook.com>
2026-02-27 18:19:57 -0700

    DAO Stuff

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,5 +1,13 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
-    
+    void addAuth(AuthData authData);
+
+    void deleteAuth(AuthData authData);
+
+    boolean validateAuth(AuthData authData);
+
+    void clearData();
 }
--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,13 +1,21 @@
 package dataaccess;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import model.UserData;
 
 public class MemoryUserDAO implements UserDAO{
-    public void findUser(String username){
-
+    Map<String, UserData> dataBase = new HashMap<>(); 
+    public UserData findUser(String username){
+        return dataBase.get(username);
     }
 
     public void addUser(UserData userData){
+        dataBase.put(userData.getUsername(), userData);
+    }
 
+    public void clear(){
+        dataBase = new HashMap<>(); 
     }
 }
