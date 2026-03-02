83b408fa762c4ebd2d5e7136aaa5f0083c7004b7
Fiery51 <tylerjackmo@outlook.com>
2026-03-02 16:08:18 -0700

    Correct method

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -5,12 +5,16 @@ import service.UserService;
 
 import java.util.Map;
 
+import com.google.gson.Gson;
+
 import dataaccess.DataAccessException;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
 import io.javalin.*;
 import io.javalin.http.ConflictResponse;
+import model.AuthData;
+import model.UserData;
 
 public class Server {
 
@@ -22,29 +26,53 @@ public class Server {
         MemoryGameDAO memoryGame = new MemoryGameDAO();
 
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        var serializer = new Gson();
 
         // Register your endpoints and exception handlers here.
         javalin.post("/user", ctx -> {
             try{
-                String username = ctx.bodyAsClass(Map.class).get("username").toString();
-                new UserService().getUser(username, memoryAuth, memoryUser, memoryGame);
+                var body = serializer.fromJson(ctx.body(), Map.class);
+                String username = body.get("username").toString();
+                String password = body.get("password").toString();
+                String email = body.get("email").toString();
+                String authToken = UserService.generateToken();
+
+                //see if the user exists
+                new UserService().getUser(username, password, email, memoryUser);
+
+                //create the user if not
+                new UserService().createUser(new UserData(username, password, email), memoryUser);
+                
+                //create the auth token
+                new UserService().createAuth(new AuthData(authToken, username), memoryAuth);
+                
                 ctx.status(200);
+                var obj = Map.of("username", username,"authToken", authToken);
+                ctx.result(serializer.toJson(obj));
+
+                
+
             }
 
             catch(IllegalArgumentException e){
                 ctx.status(400);
-                ctx.result("Error:" + e);
+                var obj = Map.of("message", "Error:" + e);
+                ctx.result(serializer.toJson(obj));
             }
 
             catch(ConflictResponse e){
                 ctx.status(403);
-                ctx.result("Error:" + e);
+                var obj = Map.of("message", "Error:" + e);
+                ctx.result(serializer.toJson(obj));
             }
 
             catch (DataAccessException e){
                 ctx.status(500);
-                ctx.result("Error:" + e);
+                var obj = Map.of("message", "Error:" + e);
+                ctx.result(serializer.toJson(obj));
             }
+
+
         });
         //javalin.post("/session", null);
         //javalin.delete("session", null);
@@ -58,7 +86,8 @@ public class Server {
             }
             catch (DataAccessException e){
                 ctx.status(500);
-                ctx.result("Error:" + e);
+                var obj = Map.of("message", "Error:" + e);
+                ctx.result(serializer.toJson(obj));
             }
             
         }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,25 +1,37 @@
 package service;
 
+import java.util.UUID;
+
 import dataaccess.DataAccessException;
 import dataaccess.MemoryAuthDAO;
-import dataaccess.MemoryGameDAO;
+
 import dataaccess.MemoryUserDAO;
 import io.javalin.http.ConflictResponse;
+import model.AuthData;
 import model.UserData;
 
 public class UserService {
-    public UserData getUser(String username, MemoryAuthDAO memoryAuth, MemoryUserDAO memoryUser, MemoryGameDAO memoryGame) throws IllegalArgumentException, DataAccessException, ConflictResponse{
+    public void getUser(String username, String password, String email, MemoryUserDAO memoryUser) throws IllegalArgumentException, DataAccessException, ConflictResponse{
         UserData theUser = memoryUser.findUser(username);
 
-        if(username == null){
+        if(username == null || password == null|| email == null){
             throw new IllegalArgumentException("bad request");
         }
 
-        //if(theUser != null){
-        //    throw new ConflictResponse("username already taken");
-        //}
-        return theUser; 
-        
-        
+        if(theUser != null){
+            throw new ConflictResponse("username already taken");
+        }
+    }
+
+    public void createUser(UserData userData, MemoryUserDAO memoryUser){
+        memoryUser.addUser(userData);
+    }
+
+    public void createAuth(AuthData authData, MemoryAuthDAO memoryAuth){
+        memoryAuth.addAuth(authData);
+    }
+
+    public static String generateToken(){
+        return UUID.randomUUID().toString();
     }
 }
