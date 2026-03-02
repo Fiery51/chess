7231c7c8600eae027dfe232dfa46f255c171c36c
Fiery51 <tylerjackmo@outlook.com>
2026-03-01 22:02:16 -0700

    Why didn't this push

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,12 +1,19 @@
 package server;
 
+import service.testMethods;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
 import io.javalin.*;
+import service.testMethods;
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
+        MemoryAuthDAO memoryAuth = new MemoryAuthDAO();
+        MemoryUserDAO memoryUser = new MemoryUserDAO();
+
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
@@ -16,7 +23,9 @@ public class Server {
         //javalin.get("/game", null);
         //javalin.post("/game", null);
         //javalin.put("/game", null);
-        javalin.delete("/db", DeleteHandler.deleteData());
+        javalin.delete("/db", ctx -> new testMethods().clearData(memoryAuth, memoryUser));
+
+
     }
 
     public int run(int desiredPort) {
