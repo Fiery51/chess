a860bffc000a9db748d00cd28567f1fde88216c2
Fiery51 <tylerjackmo@outlook.com>
2026-03-03 23:43:36 -0700

    Tests should be done
    Just cleaning up the code. 17 mins ahhhhHAH;laksjd;lfkjaw;ldjkf

--- server/src/test/java/service/GameStartsWithEmptySeatsTest.java
+++ server/src/test/java/service/GameStartsWithEmptySeatsTest.java
@@ -1,5 +1,38 @@
 package service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+
 public class GameStartsWithEmptySeatsTest {
-    
+    private MemoryUserDAO userDAO;
+    private MemoryAuthDAO authDAO;
+    private MemoryGameDAO gameDAO;
+    private CreateGameService createGameService;
+
+    @BeforeEach
+    void setup(){
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+        createGameService = new CreateGameService();
+        authDAO.addAuth(new AuthData("test", "test"));
+    }
+
+    @Test
+    void createGameNoPlayers() throws DataAccessException{
+        int gameID = createGameService.createGame("test", "test", userDAO, authDAO, gameDAO);
+        GameData game = gameDAO.findGame(gameID);
+        Assertions.assertEquals("test", game.getGameName());
+        Assertions.assertNull(game.getWhiteUsername());
+        Assertions.assertNull(game.getBlackUsername());
+        
+    }
 }
