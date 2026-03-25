65b8bc52d1c4f20b4d00977dd129bff06a8ce52e
Fiery51 <tylerjackmo@outlook.com>
2026-03-25 16:02:00 -0600

    Now it doesn't crash
    timeouts don't crash

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -74,7 +74,7 @@ public class ClientMain {
 
             case 500:
                 out.print(ERASE_SCREEN);
-                out.println("Server problem (nah doesn't happen)");
+                out.println("Server problem (nah doesn't happen, trust trust im a perfect software dev you should never see thi- why are you seeing this)");
                 loggedOut(out);
                 break;
                 
@@ -114,7 +114,7 @@ public class ClientMain {
 
             case 500:
                 out.print(ERASE_SCREEN);
-                out.println("Server problem (nah doesn't happen)");
+                out.println("Server problem (nah doesn't happen, trust trust im a perfect software dev you should never see thi- why are you seeing this)");
                 loggedOut(out);
                 break;
 
--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -3,12 +3,10 @@ package client;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpConnectTimeoutException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.net.http.HttpTimeoutException;
 import java.time.Duration;
 import java.util.Map;
 
@@ -36,7 +34,7 @@ public class ServerFacade {
             response = client.send(request, BodyHandlers.ofString());;
             System.out.println(response.body());
             result = serializer.fromJson((String) response.body(), Map.class);
-        } catch (HttpTimeoutException e) {
+        } catch (Exception e) {
             returnObject[0] = "Error";
             returnObject[1] = String.valueOf(500);
             return returnObject;
@@ -58,18 +56,26 @@ public class ServerFacade {
         var data = Map.of("username", username, "password", password);
         var serializer = new Gson();
         String jsonRequest = serializer.toJson(data);
+        String[] returnObject = new String[2];
         
-        //make HTTP request to rester a user
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8080/session"))
-            .POST(BodyPublishers.ofString(jsonRequest))
-            .timeout(Duration.ofSeconds(5))
-            .build();
+        HttpResponse<?> response = null;
+        Map result = null; 
+        try {
+            HttpClient client = HttpClient.newBuilder().build();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/session"))
+                .POST(BodyPublishers.ofString(jsonRequest))
+                .timeout(Duration.ofSeconds(5))
+                .build();
 
-        HttpResponse<?> response = client.send(request, BodyHandlers.ofString());;
-        System.out.println(response.body());
-        var result = serializer.fromJson((String) response.body(), Map.class);
+            response = client.send(request, BodyHandlers.ofString());;
+            System.out.println(response.body());
+            result = serializer.fromJson((String) response.body(), Map.class);
+        } catch (Exception e) {
+            returnObject[0] = "Error";
+            returnObject[1] = String.valueOf(500);
+            return returnObject;
+        }
 
         if(response.statusCode() == 200){
             authToken = result.get("authToken").toString();
@@ -78,21 +84,25 @@ public class ServerFacade {
             authToken = "Error";
         }
 
-        String[] returnObject = new String[2];
         returnObject[0] = authToken;
         returnObject[1] = String.valueOf(response.statusCode());
         return returnObject;
     }
 
-    public void logoutRequest(String authToken) throws IOException, InterruptedException{
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8080/session"))
-            .DELETE()
-            .header("authorization", authToken)
-            .timeout(Duration.ofSeconds(5))
-            .build();
+    public int logoutRequest(String authToken) throws IOException, InterruptedException{
+        try {
+            HttpClient client = HttpClient.newBuilder().build();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/session"))
+                .DELETE()
+                .header("authorization", authToken)
+                .timeout(Duration.ofSeconds(5))
+                .build();
 
-        HttpResponse<?> response = client.send(request, BodyHandlers.ofString());
+            HttpResponse<?> response = client.send(request, BodyHandlers.ofString());
+            return response.statusCode();
+        } catch (Exception e) {
+            return 500;
+        }
     }
 }
