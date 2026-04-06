93732b55df2b7d371a03ad11b28b469f2f85771a
Fiery51 <tylerjackmo@outlook.com>
2026-04-06 01:17:05 -0600

    Help

--- client/src/main/java/client/GameUI.java
+++ client/src/main/java/client/GameUI.java
@@ -4,6 +4,8 @@ import java.io.PrintStream;
 
 import javax.print.PrintService;
 
+import static ui.EscapeSequences.*;
+
 public class GameUI {
     PrintStream out;
 
@@ -12,7 +14,32 @@ public class GameUI {
     }
 
     private static void help(PrintStream out){
+        out.print(ERASE_SCREEN);
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLUE);
+        out.print("redraw");
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+        out.println(" - to redraw the chess board");
 
+        out.print(SET_TEXT_COLOR_BLUE);
+        out.print("leave");
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+        out.println(" - to leave the chess game");
+
+        out.print(SET_TEXT_COLOR_BLUE);
+        out.print("make move");
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+        out.println(" - to make a move");
+
+        out.print(SET_TEXT_COLOR_BLUE);
+        out.print("resign");
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+        out.println(" - to resign the game");
+        
+        out.print(SET_TEXT_COLOR_BLUE);
+        out.print("highlight");
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+        out.println(" - to highlight all legal moves on the board");
     }
 
     private static void redrawChessBoard(PrintStream out){
