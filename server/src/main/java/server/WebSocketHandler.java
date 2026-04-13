c4a6a9b981e2875305edc2e4f0eb14b0c08aab68
Fiery51 <tylerjackmo@outlook.com>
2026-04-13 03:59:01 -0600

    Connection almost done

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -5,14 +5,23 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.gson.Gson;
+
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.SQLAuthDAO;
 import io.javalin.Javalin;
 import io.javalin.websocket.WsCloseContext;
 import io.javalin.websocket.WsConnectContext;
+import io.javalin.websocket.WsContext;
 import io.javalin.websocket.WsMessageContext;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+import websocket.messages.ServerMessage.ServerMessageType;
 
 public class WebSocketHandler {
     //Game id's, then we have a set of strings that are the authtokens of everyone who is connected to the current game?
-    Map<Integer, Set<WsConnectContext>> connectionMap = new HashMap<>();
+    Map<Integer, Set<WsContext>> connectionMap = new HashMap<>();
     public static void main(String[] args){
         Javalin.create()
             .ws("/ws", ws -> {
@@ -34,27 +43,65 @@ public class WebSocketHandler {
         System.out.println("Closed!");
     }
 
-    public void onMessage(WsMessageContext ctx) {
+    public void onMessage(WsMessageContext ctx) throws DataAccessException {
+        var serializer = new Gson();
+        var message = serializer.fromJson(ctx.message(), UserGameCommand.class);
+        UserGameCommand.CommandType commandType = message.getCommandType(); 
+        switch (commandType) {
+            case CONNECT:
+                //what do we pass in for the 2nd thing here? We pass in the connection, but i have no clue what the syntax is for that?
+                addConnection(message.getGameID(), ctx);
+                loadGame(message.getGameID(), ctx);
+                //im thinking somehow we just pass in the username of the user who joined
+                broadcastNotification(message.getGameID(), message.getAuthToken());
+                break;
+            case MAKE_MOVE:
+
+                break;
+        }
+        System.out.println(message);
         System.out.println("We got a message from da client! What? idk im too lazy to print it out man");
         
     }
 
-    public void addConnection(int gameID, WsConnectContext connection){
-        Set<WsConnectContext> set = connectionMap.get(gameID);
+    public void addConnection(int gameID, WsContext connection){
+        Set<WsContext> set = connectionMap.get(gameID);
         if(set == null){
-            set = new HashSet<WsConnectContext>();
+            set = new HashSet<WsContext>();
             connectionMap.put(gameID, set);
         }
         set.add(connection);
     }
 
-    public void removeConnection(int gameID, WsConnectContext connection){
-        Set<WsConnectContext> set = connectionMap.get(gameID);
+    public void removeConnection(int gameID, WsContext connection){
+        Set<WsContext> set = connectionMap.get(gameID);
         set.remove(connection);
     }
 
     void redrawChessBoardBroadcast(int gameID, String json){
-        Set<WsConnectContext> set = connectionMap.get(gameID);
+        Set<WsContext> set = connectionMap.get(gameID);
+        for(var client : set){
+            client.send(json);
+        }
+    }
+
+    void loadGame(int gameID, WsContext client){
+        //we want the connection to the specific person
+        //i'm just gonna have them do redraw board preetty simple, its built out and ready
+        //for us. We although need to direct them to WHICH game though? 
+        var serializer = new Gson();
+        var message = Map.of(gameID, new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME))
+        var json = serializer.toJson(message);
+        client.send(json);
+    }
+
+    void broadcastNotification(int gameID, String authToken) throws DataAccessException{
+        Set<WsContext> set = connectionMap.get(gameID);
+        var serializer = new Gson();
+        SQLAuthDAO authDAO = new SQLAuthDAO();
+        String username = authDAO.getUsername(authToken);
+        String message =  username + " joined the game";
+        var json = serializer.toJson(message);
         for(var client : set){
             client.send(json);
         }
